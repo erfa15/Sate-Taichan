64bf135c91901bdf8c5ad99b4eef0c86b2936ef3
erfa15 <127957256+erfa15@users.noreply.github.com>
2023-07-11 10:58:58 +0700

    Add files via upload

---
+++ config/HOWTOPAY.js
@@ -0,0 +1,26 @@
+export const HOWTOPAY = [
+  {
+    id: 1,
+    title: "Pilih Produk",
+    desc: "Pilih produk yang anda inginkan misal, disini kami akan memilih, Sate Taichan Ayam 5 tusuk, Sate Taichan Kulit 5 tusuk dan Lontong 1 buah",
+    img: "/imgs/howtopay/howtopay-1.png",
+  },
+  {
+    id: 2,
+    title: "Cart Produk",
+    desc: "Silahkan tambah atau kurang produk yang ingin dibeli, apabila sudah yakin, silahkan pencet tombol Bayar Sekarang.",
+    img: "/imgs/howtopay/howtopay-2.png",
+  },
+  {
+    id: 3,
+    title: "Form Pembayaran",
+    desc: "Periksa apakah produk sudah sesuai dengan apa yang di pesan, apabila sudah maka isi form tersebut sesuai data diri anda.",
+    img: "/imgs/howtopay/howtopay-3.png",
+  },
+  {
+    id: 4,
+    title: "Beralih ke Whatsapp",
+    desc: "Anda tinggal memencet tombol Kirim, dan produk yang anda inginkan secara otomatis akan dikirim ke Admin! dan tinggal tunggu konfirmasi!",
+    img: "/imgs/howtopay/howtopay-4.png",
+  },
+];
